6c21483dd66fb4c48648f4991f014811ee18cf5d
Maureen <morynmoh@gmail.com>
2023-03-28 16:25:30 +0300

    Completed string methods

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+//const welcomeMessage = "Welcome to FlatBook, " + currentUser + `!`;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//const welcomeMessage = 'Welcome to Flatbook, ';
+
+//const welcomeMessage = currentUser;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}"!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
